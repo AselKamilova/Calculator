524ae9bd3130c0e10d7f0a99c3e40729a932b347
Asel <stutdentcyber@gmail.com>
2019-01-19 16:58:48 -0500

    message

--- src/Claculator.java
+++ src/Claculator.java
@@ -8,8 +8,17 @@ public class Claculator {
         double result =num1-num2;
         return result;
     }
+
     
     public static double multiply (double num1, double num2){
         return num1*num2;
+
+
+
+
+
+}
+public  static double divide(double n1,double n2){
+        return n1/n2;
 }
 }
